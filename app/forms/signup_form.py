8ea07f8efe35e60b7a3ddb75b8aebb798f698320
Jennifer Lee <leejpac@gmail.com>
2023-08-08 19:24:25 -0400

    sign up form update

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -19,9 +19,19 @@ def username_exists(form, field):
     if user:
         raise ValidationError('Username is already in use.')
 
-
 class SignUpForm(FlaskForm):
-    username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+    username = StringField("username", validators=[DataRequired(), username_exists])
+    email = StringField("email", validators=[DataRequired(), user_exists])
+    first_name = StringField(
+        "first_name",
+        validators=[
+            DataRequired(),
+        ],
+    )
+    last_name = StringField(
+        "last_name",
+        validators=[
+            DataRequired(),
+        ],
+    )
+    password = StringField("password", validators=[DataRequired()])
