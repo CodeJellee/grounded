135bf8a0eaea2cc8c64aa54181e850f3da5986e7
Jennifer Lee <leejpac@gmail.com>
2023-08-09 14:16:23 -0400

    get product by productId, get user's products

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask_login import login_required, current_user
-from app.models import db, User, Product
+from app.models import db, User, Product, ProductImage, CartItem
 from app.forms import NewProduct, NewProductImage, NewArticle, NewWorkshop
 from sqlalchemy import insert
 from pprint import pprint
@@ -9,7 +9,7 @@ product_routes = Blueprint("products", __name__)
 
 # prefix /products
 
-#GET all Products
+#GET all Products -checked on postman
 @product_routes.route("/")
 def get_products():
     dictionaryDataResponse = {}
@@ -23,3 +23,30 @@ def get_products():
         product["Seller"] = seller
 
     return dictionaryDataResponse
+
+
+#GET Single Product- checked on postman
+@product_routes.route("/<int:id>")
+def product_info(id):
+    product = Product.query.get(id)
+    if not product:
+        return {"message": "Product couldn't be found."}
+    product = product.to_dict()
+    sellerId = product["sellerId"]
+    seller = User.query.get(sellerId).to_dict()
+    product_images = ProductImage.query.filter(ProductImage.productId == id)
+    product_images = [each.to_dict() for each in product_images]
+    product["Seller"] = seller
+    product["ProductImages"] = product_images
+    return product
+
+
+#GET Products sold by user -checked on postman
+@product_routes.route("/current")
+@login_required
+def current_user_products():
+    # pprint('THIS IS CURRENT_USER', current_user)
+    curr_user_id = current_user.to_dict()["id"]
+    # pprint('THIS IS CURRENT_USER_ID', curr_user_id)
+    user_products = Product.query.filter(Product.sellerId == curr_user_id).all()
+    return {"Products": [p.to_dict() for p in user_products]}
