43760f4293fd73596facc5930e2741cfa1d64173
Jennifer Lee <leejpac@gmail.com>
2023-08-11 23:32:45 -0400

    seeder data cart complete, runs in dev, checking in prod

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,6 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .products import seed_products, undo_products
+from .cart_items import seed_cart_items, undo_cart_items
 
 from app.models.db import db, environment, SCHEMA
 
@@ -17,16 +18,19 @@ def seed():
         # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_cart_items
         undo_products()
         undo_users()
     users = seed_users()
-    seed_products(users)
+    products = seed_products(users)
+    seed_cart_items(users, products)
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_cart_items
     undo_products()
     undo_users()
     # Add other undo functions here
