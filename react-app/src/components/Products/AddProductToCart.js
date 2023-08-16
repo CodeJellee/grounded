53c66bb78eec41ba4079624163838efd4f6d5f8e
Jennifer Lee <leejpac@gmail.com>
2023-08-16 12:05:52 -0400

    error messages for adding into cart update

--- react-app/src/components/Products/AddProductToCart.js
+++ react-app/src/components/Products/AddProductToCart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { thunkPostItemToCart } from "../../store/cart";
+import { thunkGetCurrentCart, thunkPostItemToCart } from "../../store/cart";
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import { useHistory } from "react-router-dom";
 
@@ -8,20 +8,23 @@ function AddProductToCart({productId}) {
 
     const dispatch = useDispatch()
     const history = useHistory();
-    const currentUser = useSelector(state => state.session.user)
-    const product = useSelector(state => state.products.singleProduct)
-    const currentCart = useSelector(state => state.carts)
+    const sessionUser = useSelector((state) => state.session.user)
+    const productState = useSelector((state) => (state.products.singleProduct))
+    const currentCart = useSelector((state) => Object.values(state.cart.currentCart))
+    console.log('WHAT IS CURRENT CART', currentCart)
 
     const productBelongsToUser = () => {
-        return product.sellerId === currentUser.id
+        console.log('WHAT IS PRINTING OUT HERE', productState.sellerId, sessionUser.id)
+        return productState.sellerId === sessionUser.id
     };
 
-    let currentCartItems = Object.values(currentCart.currentCart) //ERROR HERE
 
     const productAlreadyInCart = () => {
-
-        for (const item of currentCartItems) {
-            if (item.productId === productId) {
+        console.log('IS THIS HITTING', currentCart, productId)
+        for (const item of currentCart) {
+            console.log('WHAT IS ITEM IN CURRENTCART FOR LOOP', item)
+            if (item.productId === productState.id) {
+                console.log('ARE THE ITEMS ALREADY IN TEH CART', item.productId, productState.id)
                 return true
             }
 
@@ -29,19 +32,20 @@ function AddProductToCart({productId}) {
         return false;
     }
 
-    const onClick = (e) => {
+    const onClick = async (e) => {
         e.preventDefault();
         if (productBelongsToUser()) {
             alert("You own this product!");
             return;
         }
+
         if (productAlreadyInCart()) {
             alert("Product is already in your cart!");
-            return;
-        }
-        else {
 
-            dispatch(thunkPostItemToCart(product.id))
+        } else {
+
+            await dispatch(thunkPostItemToCart(productState.id));
+            await dispatch(thunkGetCurrentCart());
             history.push("/carts")
 
         }
