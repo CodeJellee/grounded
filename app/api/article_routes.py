73db68391b99ca23e758628551d9a8aa7c024ea7
Jennifer Lee <leejpac@gmail.com>
2023-09-17 15:53:42 -0400

    article get all, by id, by user and delete done and checked on postman

--- app/api/article_routes.py
+++ app/api/article_routes.py
@@ -9,7 +9,7 @@ article_routes = Blueprint("articles", __name__)
 
 # prefix /articles
 
-#GET all articles
+#GET all articles-checked on postman
 @article_routes.route("/", methods=["GET"])
 def get_articles():
     dictionaryDataResponse = {}
@@ -23,3 +23,44 @@ def get_articles():
         article["Author"] = author
 
     return dictionaryDataResponse
+
+
+#GET Single article-checked on postman
+@article_routes.route("/<int:id>", methods=["GET"])
+def article_info(id):
+    article = Article.query.get(id)
+
+    if not article:
+        return {"message": "Article couldn't be found."}
+
+    article = article.to_dict()
+    author = User.query.get(article["authorId"])
+    author = author.to_dict()
+    article["Author"] = author
+
+    return article
+
+
+#GET Articles by user -checked on postman
+@article_routes.route("/current", methods=["GET"])
+@login_required
+def current_user_articles():
+    curr_user_id = current_user.to_dict()["id"]
+    user_articles = Article.query.filter(Article.authorId == curr_user_id).all()
+
+    if not user_articles:
+        return {"message": "No articles found for the current user."}, 404
+
+    return {"Articles": [a.to_dict() for a in user_articles]}
+
+
+#DELETE article by user-checked on postman
+@article_routes.route("/<int:id>", methods = ["DELETE"])
+@login_required
+def delete_article(id):
+    article_to_delete = Article.query.get(id)
+    if not article_to_delete:
+        return {"message": "Article couldn't be found."}
+    db.session.delete(article_to_delete)
+    db.session.commit()
+    return {"message": "Article successfully deleted!"}
