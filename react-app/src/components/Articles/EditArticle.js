14179370f8a089d158fa884fa7932c1a315593cc
Jennifer Lee <leejpac@gmail.com>
2023-09-19 17:55:16 -0400

    user delete article functionality added

--- react-app/src/components/Articles/EditArticle.js
+++ react-app/src/components/Articles/EditArticle.js
@@ -1,5 +1,16 @@
+import { thunkDeleteSingleArticle } from "../../store/article";
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
 
 function EditArticle({article}) {
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const deleteClick = async () => {
+        await dispatch(thunkDeleteSingleArticle(article.id))
+        history.push(`/articles/current`)
+    }
 
     return (
         <div className="main-single-article-container">
@@ -7,7 +18,7 @@ function EditArticle({article}) {
             <div className="each-article-description">{article.article_description}</div>
             <div className="each-article-buttons">
                 <button className="user-article-edit-delete-buttons">Edit</button>
-                <button className="user-article-edit-delete-buttons">Delete</button>
+                <button className="user-article-edit-delete-buttons" onClick={deleteClick}>Delete</button>
             </div>
         </div>
     )
