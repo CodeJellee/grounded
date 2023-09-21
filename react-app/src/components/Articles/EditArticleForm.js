963a4447f5504511803a62f1af9d93e15df7f1d6
Jennifer Lee <leejpac@gmail.com>
2023-09-20 20:35:40 -0400

    error message for article update/create update

--- react-app/src/components/Articles/EditArticleForm.js
+++ react-app/src/components/Articles/EditArticleForm.js
@@ -68,7 +68,7 @@ const EditArticleForm = () => {
         }
 
         if (articleTitle.length > 125) {
-            errorsObject.articleTitle = "Less than 225 characters required.";
+            errorsObject.articleTitle = "Less than 125 characters required.";
         }
 
         if (articleDescription === "") {
